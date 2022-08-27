eac75886433c95f61a52cc2831768676651c3f99
Qiwei Huang <qiwei_huang@qq.com>
2022-08-27 19:29:04 +0800

    Add reflection

--- RayTracingOneWeek/Materials/DielectricMaterial.cs
+++ RayTracingOneWeek/Materials/DielectricMaterial.cs
@@ -17,9 +17,24 @@ public class DielectricMaterial : IMaterial
         var refractionRatio = rec.FrontFace ? (1.0 / IndexOfRefraction) : IndexOfRefraction;
 
         var unitDirection = rIn.Direction.UnitVector();
-        var refracted = Vec3.Refract(unitDirection, rec.Normal, refractionRatio);
+        var cosTheta = Math.Min(Vec3.Dot(-unitDirection, rec.Normal), 1.0);
+        var sinTheta = Math.Sqrt(1.0 - cosTheta * cosTheta);
 
-        scattered = new Ray(rec.P, refracted);
+        var cannotRefract = refractionRatio * sinTheta > 1.0;
+
+        var direction = cannotRefract || reflectance(cosTheta, refractionRatio) > Utility.RandomDouble()
+            ? Vec3.Reflect(unitDirection, rec.Normal)
+            : Vec3.Refract(unitDirection, rec.Normal, refractionRatio);
+
+        scattered = new Ray(rec.P, direction);
         return true;
+    }
+
+    private static double reflectance(double cosine, double refIdx)
+    {
+        // Use Schlick's approximation for reflectance.
+        var r0 = (1 - refIdx) / (1 + refIdx);
+        r0 *= r0;
+        return r0 + (1 - r0) * Math.Pow((1 - cosine), 5);
     }
 }
--- RayTracingOneWeek/Program.cs
+++ RayTracingOneWeek/Program.cs
@@ -47,13 +47,14 @@ public static class Program
         //World
         var world = new HitTableList();
         var materialGround = new DiffuseMaterial(new Color(0.8, 0.8, 0.0));
-        var materialCenter = new DielectricMaterial(1.5);
+        var materialCenter = new DiffuseMaterial(new Color(0.1, 0.2, 0.5));
         var materialLeft = new DielectricMaterial(1.5);
-        var materialRight = new MetalMaterial(new Color(0.8, 0.6, 0.2), 1.0);
+        var materialRight = new MetalMaterial(new Color(0.8, 0.6, 0.2), 0);
 
         world.Add(new Sphere(new Point3(0, -100.5, -1), 100, materialGround));
         world.Add(new Sphere(new Point3(0, 0, -1), 0.5, materialCenter));
         world.Add(new Sphere(new Point3(-1, 0, -1), 0.5, materialLeft));
+        world.Add(new Sphere(new Point3(-1, 0, -1), -0.4, materialLeft));
         world.Add(new Sphere(new Point3(1, 0, -1), 0.5, materialRight));
 
         //Camera
